4881f0a743de931529053041aa1951e0c92c746d
Katy James <katy.james@levvel.io>
2016-09-05 21:03:29 -0400

    placing google maps correctly

--- src/client/containers/details.js
+++ src/client/containers/details.js
@@ -21,51 +21,46 @@ class Details extends React.Component {
     const { markers } = this.props
 
     return(
-      <div className="details">
-        <Nav location="details"/>
-
-
-        <GoogleMapLoader
-          containerElement={
-            <div style={{
-                height: 200,
-                width: 200 }}/> }
-          googleMapElement={
-            <GoogleMap
-              defaultOptions={{
-                draggable: false,
-                zoomControl: false,
-                scrollwheel: false,
-                disableDoubleClickZoom: true
-              }}
-              defaultZoom={ 15 }
-              defaultCenter={{
-                lat: 35.329155,
-                lng: -80.634645,
-              }}
-              onClick={ this.onMapClick }>
-              {markers.map((marker, index) => {
-                return (
-                  <Marker
-                    draggable={ false }
-                    {...marker}
-                    onRightclick={() => this.onMarkerRightclick(index) } />
-                );
-              })}
-            </GoogleMap>
-          }/>
-
-
-
       <div>
         <div className="details">
           <Nav location="details"/>
         </div>
         <div className="kraft-section">
-          <div className="callout">
-            <h2>we are so honored that you are able to join us on our special day!</h2>
+          <div className="vertical-align-wrap">
+            <div className="vertical-align vertical-align--middle">
+              <h2>we are so honored that you are able to join us on our special day!</h2>
+            </div>
           </div>
         </div>
+          <GoogleMapLoader
+            containerElement={
+              <div style={{
+                  height: 200,
+                  width: '100%' }}/> }
+            googleMapElement={
+              <GoogleMap
+                defaultOptions={{
+                  draggable: false,
+                  zoomControl: false,
+                  scrollwheel: false,
+                  disableDoubleClickZoom: true
+                }}
+                defaultZoom={ 15 }
+                defaultCenter={{
+                  lat: 35.329155,
+                  lng: -80.634645,
+                }}
+                onClick={ this.onMapClick }>
+                {markers.map((marker, index) => {
+                  return (
+                    <Marker
+                      draggable={ false }
+                      {...marker}
+                      onRightclick={() => this.onMarkerRightclick(index) } />
+                  );
+                })}
+              </GoogleMap>
+            }/>
       </div>
     )
   }

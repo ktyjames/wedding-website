00deec582996b3a6203a6bf0937a86c59b472d08
Phil <phil3903@gmail.com>
2016-09-05 20:49:30 -0400

    Added google maps to Details

--- src/client/containers/details.js
+++ src/client/containers/details.js
@@ -1,16 +1,62 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Nav from '../components/nav'
+import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps"
 
 class Details extends React.Component {
   constructor(props){
     super(props)
   }
 
+  onMapClick =()=>{
+
+  }
+
+  onMarkerRightclick =()=>{
+
+  }
+
   render(){
+
+    const { markers } = this.props
+
     return(
       <div className="details">
         <Nav location="details"/>
+
+
+        <GoogleMapLoader
+          containerElement={
+            <div style={{
+                height: 200,
+                width: 200 }}/> }
+          googleMapElement={
+            <GoogleMap
+              defaultOptions={{
+                draggable: false,
+                zoomControl: false,
+                scrollwheel: false,
+                disableDoubleClickZoom: true
+              }}
+              defaultZoom={ 15 }
+              defaultCenter={{
+                lat: 35.329155,
+                lng: -80.634645,
+              }}
+              onClick={ this.onMapClick }>
+              {markers.map((marker, index) => {
+                return (
+                  <Marker
+                    draggable={ false }
+                    {...marker}
+                    onRightclick={() => this.onMarkerRightclick(index) } />
+                );
+              })}
+            </GoogleMap>
+          }/>
+
+
+
       </div>
     )
   }
@@ -19,7 +65,7 @@ class Details extends React.Component {
 
 function mapStateToProps(state){
   return {
-
+    markers: state.details.markers
   }
 }
 

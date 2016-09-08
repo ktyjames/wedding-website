bfe54d5ada99ea8cc6394ffaf8a57ba029dd53ac
Phil <phil3903@gmail.com>
2016-09-08 17:17:01 -0400

    Removed ie11 compatibility

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -58,7 +58,6 @@ function renderFullPage(html, initialState) {
     <!DOCTYPE html>
     <html>
       <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=11">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" media="all" />
         ${ googlePath }
